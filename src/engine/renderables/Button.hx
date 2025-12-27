dc530b6604396e000123e82416bd8c1415bcd46b
GenieCMD <mrgenie151@gmail.com>
2025-12-27 17:34:45 -0500

    Finished button implementation

--- src/engine/renderables/Button.hx
+++ src/engine/renderables/Button.hx
@@ -11,8 +11,10 @@ class Button extends Renderable2D {
 	public var spacing : Float = 10;
 	public var color : Color = new Color(255,255,255,255);
 	public var size : Vector2;
+
+	public var clicked = false;
 	
-	public function new(text,x = 0,y = 0,xsize = 100, ysize = 500,font = null,textsize = 16) {
+	public function new(text,x = 0,y = 0,xsize = 100, ysize = 500,font = null,textsize = 16.0) {
 		this.text = text;
 		this.position = new Vector2(x,y);
 		this.font = font;
@@ -23,10 +25,22 @@ class Button extends Renderable2D {
 	override function draw() {
 		DrawRectangleV(position,size,BLACK);
 
+		var textSizeish = MeasureTextEx(font,text,textsize,spacing);
+		var textPosition = new Vector2((size.x / 2) - (textSizeish.x / 2), (size.y / 2) - (textSizeish.y / 2));
+
 		if (font != null)
-			DrawTextEx(font,text,position,textsize,spacing,color);
+			DrawTextEx(font,text,textPosition,textsize,spacing,color);
 		else
-			DrawText(text,Std.int(position.x),Std.int(position.y),Std.int(textsize),color);
+			DrawText(text,Std.int(textPosition.x),Std.int(textPosition.y),Std.int(textsize),color);
+	}
+
+	override function update(delta) {
+		var rect = new Rectangle(position.x,position.y,size.x,size.y);
+		var mousePos = GetMousePosition();
+		if (CheckCollisionPointRec(mousePos,rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+			clicked = true;
+		else
+			clicked = false;
 	}
 
 }
